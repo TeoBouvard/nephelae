ab02a84e113f5507f27a22d4a99c645656cc9e2a
Téo Bouvard <teobouvard@gmail.com>
2019-06-17 16:18:53 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,36 @@
 # nephelae
+
+## TODO
+
+## Questions
+
+-   authentication ?
+-   approximate mesh from points cloud
+
+## Ideas
+
+-   3D global view :
+
+    -   axes (cardinal + height + time)
+    -   drones + trails + infos?
+    -   clouds (lwc > 0)
+    -   sélection plan de coupe
+
+-   Vues en coupe :
+
+    -   horizontale (altitude)
+    -   verticale (vecteur normal)
+
+-   Mesures
+
+    -   emagrammes
+    -   infos drones
+
+## Libs
+
+-   Cesium (ok mais fort couplage maps)
+-   I3S bof
+-   Flyvast (\$\$)
+-   three.js (pas mal, doc++)
+-   Potree (open source, doc--, win--)
+-   PPTK
---
+++ data/quick-display.py
@@ -0,0 +1,163 @@
+#! /usr/bin/python3
+
+import os
+import time
+import numpy as np
+import matplotlib.pyplot as plt
+from   matplotlib import animation
+
+from netCDF4 import MFDataset
+
+def update_ticks_labelXY(ax):
+    newLabels = []
+    for tick in ax.get_xticks():
+        if tick < 0 or tick >= atmShape.x:
+            newLabels.append(str(0.0))
+        else:
+            newLabels.append(str(atm.variables['W_E_direction'][tick]))
+    ax.set_xticklabels(newLabels)
+    newLabels = []
+    for tick in ax.get_yticks():
+        if tick < 0 or tick >= atmShape.y:
+            newLabels.append(str(0.0))
+        else:
+            newLabels.append(str(atm.variables['S_N_direction'][tick]))
+    ax.set_yticklabels(newLabels)
+
+def update_ticks_labelXZ(ax):
+    newLabels = []
+    for tick in ax.get_xticks():
+        if tick < 0 or tick >= atmShape.x:
+            newLabels.append(str(0.0))
+        else:
+            newLabels.append(str(atm.variables['W_E_direction'][tick]))
+    ax.set_xticklabels(newLabels)
+    newLabels = []
+    for tick in ax.get_yticks():
+        if tick < 0 or tick >= atmShape.z:
+            newLabels.append(str(0.0))
+        else:
+            newLabels.append(str(atm.variables['VLEV'][tick]))
+    ax.set_yticklabels(newLabels)
+
+
+atm = MFDataset('/Users/arthurdent/Documents/dev/nephelae/data/data.nc')
+
+atmShape = type('AtmShape', (), {})()
+atmShape.t = len(atm.variables['time'][:])
+atmShape.z = len(atm.variables['VLEV'][:,0,0])
+# atmShape.z = len(atm.variables['vertical_levels'][:])
+atmShape.x = len(atm.variables['W_E_direction'][:])
+atmShape.y = len(atm.variables['S_N_direction'][:])
+
+print("Shape : (", atmShape.t, atmShape.z, atmShape.x, atmShape.y, ")")
+
+var0 = 'RCT'
+# var1 = 'UT'     # WE wind (?)
+# var1 = 'VT'     # SN wind (?)
+var1 = 'WT'     # vertical wind
+# var1 = 'TKET'   # Turbulent kinetic energy
+# var1 = 'PABST'  # Absolute pressure
+# var1 = 'RVT'    # Vapor mixing ratio
+# var1 = 'RRT'    # Rain mixing ratio
+# var1 = 'SVT001' # User data (?)
+# var1 = 'SVT002' # User data (?)
+# var1 = 'SVT003'  # User data (?)
+
+# var0 = 'objets'
+# var1 = 'objets'
+
+z0 = 44
+y0 = 175
+# y0 = 25
+rctTh = 1e-4
+
+print('Started !')
+
+fig, axes = plt.subplots(2,2, sharex=True)
+varDisp0 = axes[0][0].imshow(atm.variables[var0][0,z0,:,:])
+axes[0][0].invert_yaxis()
+varDisp1 = axes[0][1].imshow(atm.variables[var1][0,z0,:,:])
+axes[0][1].invert_yaxis()
+
+varDisp2 = axes[1][0].imshow(atm.variables[var0][0,:,:,y0])
+axes[1][0].invert_yaxis()
+varDisp3 = axes[1][1].imshow(atm.variables[var1][0,:,:,y0])
+axes[1][1].invert_yaxis()
+
+# update_ticks_labelXY(axes[0][0])
+# update_ticks_labelXY(axes[0][1])
+# update_ticks_labelXZ(axes[1][0])
+# update_ticks_labelXZ(axes[1][1])
+
+t0 = atm.variables['time'][0]
+tStart = -1
+
+tStart = time.time()
+oldData = [atm.variables[var0][0,z0,:,:],
+           atm.variables[var1][0,z0,:,:],
+           atm.variables[var0][0,:,y0,:],
+           atm.variables[var1][0,:,y0,:]]
+oldIndex = 0
+
+newData = [atm.variables[var0][1,z0,:, :],
+           atm.variables[var1][1,z0,:,:],
+           atm.variables[var0][1,:,y0,:],
+           atm.variables[var1][1,:,y0,:]]
+newIndex = 1
+def init():
+
+    varDisp0.set_data(atm.variables[var0][0,z0,:,:])
+    # varDisp0.set_data(np.where(atm.variables[var0][0,z0,:,:] > rctTh, 0.0, 1.0))
+    varDisp1.set_data(atm.variables[var1][0,z0,:,:])
+    varDisp2.set_data(atm.variables[var0][0,:,y0,:])
+    varDisp3.set_data(atm.variables[var1][0,:,y0,:])
+
+    axes[0][0].plot([0, atmShape.x -1 ], [y0, y0])
+    axes[0][1].plot([0, atmShape.x -1 ], [y0, y0])
+    axes[1][0].plot([0, atmShape.x -1 ], [z0, z0])
+    axes[1][1].plot([0, atmShape.x -1 ], [z0, z0])
+
+def update(i):
+
+    global oldIndex
+    global newIndex
+    global oldData
+    global newData
+    global tStart
+
+    # t = time.time() - tStart
+    # t = 10*time.time() - tStart
+    # r = t / 2.0 - oldIndex
+    # r = t / 5.0 - oldIndex
+
+    # if r > 1.0:
+    #     oldIndex = newIndex
+    #     newIndex = oldIndex + 1
+    #     oldData = newData
+    #     newData = [atm.variables[var0][newIndex % t,z0,:,:],
+    #                atm.variables[var1][newIndex % t,z0,:,:],
+    #                atm.variables[var0][newIndex % t,:,y0,:],
+    #                atm.variables[var1][newIndex % t,:,y0,:]]
+    #     r = r - 1.0
+
+    # varDisp0.set_data((1.0-r)*oldData[0] + r*newData[0])
+    # varDisp1.set_data((1.0-r)*oldData[1] + r*newData[1])
+    # varDisp2.set_data((1.0-r)*oldData[2] + r*newData[2])
+    # varDisp3.set_data((1.0-r)*oldData[3] + r*newData[3])
+
+    index = i % atmShape.t
+    varDisp0.set_data(atm.variables[var0][index,z0,:,:])
+    varDisp1.set_data(atm.variables[var1][index,z0,:,:])
+    varDisp2.set_data(atm.variables[var0][index,:,y0,:])
+    varDisp3.set_data(atm.variables[var1][index,:,y0,:])
+    # varDisp0.set_data(np.where(atm.variables[var0][index,z0,:,:] > rctTh, 0.0, 1.0))
+
+anim = animation.FuncAnimation(
+    fig,
+    update,
+    init_func=init,
+    frames=atmShape.x*atmShape.y,
+    interval = 500)
+
+plt.show(block=False)
