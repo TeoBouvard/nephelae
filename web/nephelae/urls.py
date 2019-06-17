db746ee5b1e565ff4faae01725a676062d759a0f
Téo Bouvard <teobouvard@gmail.com>
2019-06-17 18:05:32 +0200

    web structure

--- web/nephelae/urls.py
+++ web/nephelae/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
+from django.views.generic import RedirectView
 
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', RedirectView.as_view(url='preview')),
+    path('preview/', views.preview, name='preview'),
+    path('cross-sections/', views.cross_section, name='cross-sections'),
+    path('infos/', views.infos, name='infos'),
 ]
--- web/nephelae/views.py
+++ web/nephelae/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpRequest, HttpResponse
 
 # Create your views here.
 
-def index(request):
-    return render(request, 'nephelae/index.html')
+def preview(request):
+    return render(request, 'nephelae/preview.html')
+
+def cross_section(request):
+    return render(request, 'nephelae/cross_section.html')
+
+def infos(request):
+    return render(request, 'nephelae/infos.html')
