dfb5326bcc835fb54f6a9956d745684084b218bd
Téo Bouvard <teobouvard@gmail.com>
2019-07-02 17:14:36 +0200

    3d tracking working, have to fix axes and drag

--- web/nephelae/static/nephelae/static/js/preview.js
+++ web/nephelae/static/nephelae/static/js/preview.js
@@ -6,7 +6,15 @@ var layout = {
             scene: {
                 xaxis:{title: 'Longitude',showgrid: false}, // range: [1.2, 1.3],},
                 yaxis:{title: 'Latitude', showgrid: false}, //range: [43.4, 43.5],},
-                zaxis:{title: 'Altitude', range: [0, 300],}, fixedrange: true,
+                zaxis:{title: 'Altitude', range: [0, 300],},
+		        aspectratio: {x:1.3, y:1.3, z:0.9},
+            },
+            showlegend: false,
+            margin: {
+                t: 0, //top margin
+                l: 0, //left margin
+                r: 0, //right margin
+                b: 100, //bottom margin
             },
         };
 
@@ -22,6 +30,7 @@ var isAlreadyDrawn = false;
 
 $(document).ready(function(){
     setInterval(displayDrones, refresh_rate);
+    //displayDrones();
 });
 
 function displayDrones(){
@@ -42,19 +51,37 @@ function displayDrones(){
             var past_latitudes = response[key].past_latitudes;
 
             // Update chart data with new dataset and line color corresponding to the icon
-            var update = {
+            var updatePath = {
                 type: 'scatter3d',
                 x: past_longitudes,
                 y: past_latitudes,
                 z: past_altitudes,
                 name: drone_id,
                 mode: 'lines',
-                lines:{
+                line:{
+                    color: drone_color,
+                    shape: 'spline',
+                    dash: 'dash',
+                }
+            };
+            
+            var updateMarker = {
+                type: 'scatter3d',
+                x: [drone_position[0]],
+                y: [drone_position[1]],
+                z: [drone_altitude],
+                name: drone_id,
+                mode: 'markers',
+                marker:{
                     color: drone_color,
                 }
             };
-            data.push(update);
+
+            data.push(updatePath);
+            data.push(updateMarker);
         }
+
+        //console.log(data);
 
         if(isAlreadyDrawn){
             Plotly.react('chart', data, layout, config)
@@ -69,17 +96,3 @@ function displayDrones(){
         }
     });
 }
-
-/*
-,
-
-                type: 'scatter3d',
-                x: drone_position[0],
-                y: drone_position[1],
-                z: drone_altitude,
-                name: drone_id,
-                mode: 'markers',
-                markers:{
-                    color: drone_color,
-                }
-                */
