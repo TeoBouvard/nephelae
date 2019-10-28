995659ebb21e7d019df6c2cd93d64676435d7958
Florian Seguin <fseguin@laas.fr>
2019-10-28 11:50:26 +0100

    Modification of the vertical profile buffer (still a bug though)

--- web/nephelae_gui/static/js/profiles.js
+++ web/nephelae_gui/static/js/profiles.js
@@ -23,6 +23,13 @@ var layouts = {
         margin: { l: lm, r: rm, b: bm, t: tm },
         hovermode: 'closest'
     },
+    altitude: {
+        xaxis:{title: 'Time (s)'},
+        yaxis:{title: 'Altitude (km)'},
+        height: chart_height,
+        margin: { l: lm, r: rm, b: bm, t: tm },
+        hovermode: 'closest'
+    },
 };
 var config = {
     responsive : true,
@@ -88,8 +95,10 @@ function updateData(){
         for(var variable_name in response.data[parameters.tracked_uav]){
             var positions = response.data[parameters.tracked_uav][variable_name]['positions'];
             var altitudes = [];
+            var times = [];
             for(var i = 0; i < positions.length ; i++){
                 altitudes.push(positions[i][3]);
+                times.push(positions[i][0]);
             }
             var new_data = {
                 type: 'line',
@@ -116,30 +125,72 @@ function updateData(){
             };
             variable_name in data ? data[variable_name].push(new_data) : data[variable_name] = [new_data];
         }
+        data['ALT'] = [{
+            type: 'line',
+            name: parameters.tracked_uav,
+            x: times,
+            y: altitudes,
+            mode: 'line',
+            line: {
+                width: 1,
+                shape: 'linear',
+                color: global_colors[parameters.tracked_uav%global_colors.length],
+            },
+            meta: [parameters.tracked_uav],
+            hovertemplate:
+                'Valeur : %{x:.1f}s <br>' +
+                'Altitude : %{y:.2f} <br>' +
+                '<extra>UAV %{meta[0]}</extra>',
+            hoverlabel: {
+                bgcolor: 'black',
+                bordercolor: 'black',
+                font: {family: 'Roboto', si1ze: '15', color: 'white'},
+                align: 'left',
+            }
+        }];
         updateCharts(data);
         if (parameters.streaming && parameters.socket == null) {
             parameters.socket = new WebSocket('ws://' + window.location.host + '/ws/sensor/');
             parameters.socket.onmessage = (e) => handleMessage(JSON.parse(e.data));
         }
-    	removeLoader();
+    removeLoader();
     });
 }
 
 function handleMessage(data){
-    var chart_name = '';
+  var chart_name = '';
 	if ((data['uav_id'] == parameters.tracked_uav)  
 		&& (parameters.variables[data.variable_name])){
+    var chart_altitude = $('#altitude_chart');
+    var first_time = chart_altitude[0].data[0].x[0];
+    list_names = ['altitude_chart', 'temperature_chart', 'humidity_chart'];
+    while (data.position[0]-first_time > parameters.trail_length){
+      list_names.forEach(function(name){
+        var document_data = $('#' + name)[0].data;
+        document_data[0].x.shift();
+        document_data[0].y.shift();
+      });
+      first_time = chart_altitude[0].data[0].x[0];
+    }
+		var altitude_trace_index = getTraceIndexByName(chart_altitude, data.uav_id);
 		if (data['variable_name'] == 'THT')
 			chart_name = 'temperature_chart';
 		else if (data['variable_name'] == 'RCT')
 			chart_name = 'humidity_chart';
 		var chart = $('#' + chart_name);
-		var trace_index = getTraceIndexByName(chart, data.uav_id)
+		var trace_index = getTraceIndexByName(chart, data.uav_id);
 		var update = {
 			y: [[data.position[3]]],
 			x: [[data.data[0]]]
 		};
 		Plotly.extendTraces(chart_name, update, [trace_index]);
+    var chart_altitude = $('#altitude_chart');
+		var altitude_trace_index = getTraceIndexByName(chart_altitude, data.uav_id);
+    var update_altitude = {
+			y: [[data.position[3]]],
+			x: [[data.position[0]]]
+    };
+		Plotly.extendTraces('altitude_chart', update_altitude, [altitude_trace_index]);
 	}
 }
 
@@ -154,6 +205,7 @@ function toggleStreaming(state){
 function updateCharts(data){
     Plotly.react('temperature_chart', data.THT, layouts.temperature, config);
     Plotly.react('humidity_chart', data.RCT, layouts.humidity, config);
+    Plotly.react('altitude_chart', data.ALT, layouts.altitude, config);
 }
 
 function getTraceIndexByName(chart, name){
