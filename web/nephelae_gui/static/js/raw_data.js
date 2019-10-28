1554c6444e0f7c4458763c7b4886b83fdbb64074
Florian Seguin <fseguin@laas.fr>
2019-10-28 18:35:23 +0100

    [vertical] Added a micro-buffer before sending data in trackers.py

--- web/nephelae_gui/static/js/raw_data.js
+++ web/nephelae_gui/static/js/raw_data.js
@@ -37,11 +37,11 @@ function setupGUI(){
     f1.add(parameters, 'trail_length', 10, 2000).step(10).name("Log length (s)").onFinishChange(updateData);
 
     var f2 = gui.addFolder('History');
-    
+
     f2.add(parameters, 'start_buff').name("Start Buffer").onChange(updateData);
     f2.add(parameters, 'end_buff').name("End Buffer").onChange(updateData);
     fieldsBehavior(parameters.streaming, f1, f2);
-    
+
     var f3 = gui.addFolder('UAVs');
     var f4 = gui.addFolder('Variables');
 
@@ -59,7 +59,7 @@ function setupGUI(){
             parameters['variables'][tag] = true;
             f4.add(parameters['variables'], tag).name(tag).onChange((state) => toggleChart(state));
         }
-    
+
         gui.add(parameters, 'streaming').name("Streaming").onChange(function(state) {
             fieldsBehavior(state, f1, f2);
             toggleStreaming(state);
@@ -76,7 +76,7 @@ function updateData(){
     var data = {};
     var query = makeQuery();
     $.getJSON('update/?' + query, (response) => {
-        
+
         // Parse server response
         for (var uav_id in response.data){
 
@@ -103,9 +103,9 @@ function updateData(){
                     },
                     meta: [uav_id],
                     hovertemplate:
-                        'Time : %{x:.1f}s <br>' +
-                        'Value : %{y:.2f} <br>' +
-                        '<extra>UAV %{meta[0]}</extra>',
+                    'Time : %{x:.1f}s <br>' +
+                    'Value : %{y:.2f} <br>' +
+                    '<extra>UAV %{meta[0]}</extra>',
                     hoverlabel: {
                         bgcolor: 'black',
                         bordercolor: 'black',
@@ -152,9 +152,9 @@ function toggleChart(state){
             if (state){
                 $('#charts').append(
                     '<div id="container_'+ variable + '" class="row">' +
-                        '<div class="col s12">' +
-                            '<div id=' + variable + '></div>' +
-                        '</div>' +
+                    '<div class="col s12">' +
+                    '<div id=' + variable + '></div>' +
+                    '</div>' +
                     '</div>'
                 );
                 updateData();
@@ -176,42 +176,44 @@ function printLayout(variable){
     };
 }
 
-function handleMessage(message){
+function handleMessage(messages){
+    //Reads the list of messages
+    for (var message of messages){
+        // if variable is selected to be visible
+        if(parameters.variables[message.variable_name]){
 
-    // if variable is selected to be visible
-    if(parameters.variables[message.variable_name]){
+            // identify chart DOM element and compute the trace's index
+            var chart = $('#'+ message.variable_name);
+            var trace_index = getTraceIndexByName(chart, message.uav_id);
 
-        // identify chart DOM element and compute the trace's index
-        var chart = $('#'+ message.variable_name);
-        var trace_index = getTraceIndexByName(chart, message.uav_id);
+            // if such a trace exists
+            if (trace_index > -1){
 
-        // if such a trace exists
-        if (trace_index > -1){
-
-            // update data
-            var document_data = document.getElementById(message.variable_name).data;
-            for(var i = 0; i < document_data.length; i++){
-              var first_time = document_data[i].x[0];
-              while (message.position[0]-first_time > parameters.trail_length){
-                  document_data[i].x.shift();
-                  document_data[i].y.shift();
-                  first_time = document_data[i].x[0];
-              }
-            }
-            var update = {
+                // update data
+                var document_data = document.getElementById(message.variable_name).data;
+                for(var i = 0; i < document_data.length; i++){
+                    var first_time = document_data[i].x[0];
+                    while (message.position[0]-first_time > parameters.trail_length){
+                        document_data[i].x.shift();
+                        document_data[i].y.shift();
+                        first_time = document_data[i].x[0];
+                    }
+                }
+                var update = {
                     x:  [[message.position[0]]],
-              
+
                     y: [[message.data[0]]]
-            };
-            Plotly.extendTraces(message.variable_name, update, [trace_index]);
-            // The following operation is very expensive, uncomment it only if you need fixed range streaming plot
-        } else {
-            // if trace index is not found, reload the page
-            //location.reload(); -> something has to be done
+                };
+                Plotly.extendTraces(message.variable_name, update, [trace_index]);
+                // The following operation is very expensive, uncomment it only if you need fixed range streaming plot
+            } else {
+                // if trace index is not found, reload the page
+                //location.reload(); -> something has to be done
+            }
+            // if variable does not exist, re-setup the page
+        } else if (!(message.variable_name in parameters.variables)) {
+            setupGUI();
         }
-    // if variable does not exist, re-setup the page
-    } else if (!(message.variable_name in parameters.variables)) {
-        setupGUI();
     }
 }
 
@@ -222,16 +224,16 @@ function getTraceIndexByName(chart, name){
 
 function makeQuery(){
     return (parameters.streaming 
-            ? 
-            $.param({start: parameters.trail_length,
-                variables: getSelectedElements(parameters.variables),
-			 	uav_id: getSelectedElements(parameters.uavs)})
-            :
-			$.param({start: -parameters.start_buff,
-                variables: getSelectedElements(parameters.variables),
-			 	uav_id: getSelectedElements(parameters.uavs),
-                step: 1,
-                end: parameters.end_buff}));
+        ? 
+        $.param({start: parameters.trail_length,
+            variables: getSelectedElements(parameters.variables),
+            uav_id: getSelectedElements(parameters.uavs)})
+        :
+        $.param({start: -parameters.start_buff,
+            variables: getSelectedElements(parameters.variables),
+            uav_id: getSelectedElements(parameters.uavs),
+            step: 1,
+            end: parameters.end_buff}));
 }
 
 function fieldsBehavior(state, f1, f2){
