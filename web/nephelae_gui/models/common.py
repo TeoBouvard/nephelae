6c0cbf245845848bf8681a7e2e0b922ac015f70c
Pierre Narvor <pnarvor@laas.fr>
2020-01-26 15:22:58 -0400

    [raw_data] Updated displayed sensors

--- web/nephelae_gui/models/common.py
+++ web/nephelae_gui/models/common.py
@@ -14,8 +14,10 @@ try:
     db = scenario.database
 
     # Defines displayable samples, to keep for now. Find an alternative to put in scenario
-    # db_data_tags = ['temperature', 'pressure', 'cloud_channel_0', 'cloud_channel_0', 'RCT', 'WT', 'THT'] 
-    db_data_tags = ['temperature', 'pressure', 'cloud_channel_0', 'cloud_channel_1'] 
+    # db_data_tags = ['RCT', 'WT', 'THT']
+    db_data_tags = ['RCT', 'WT', 'THT', 'temperature', 'pressure', 'cloud_channel_0', 'cloud_channel_1'] 
+    # db_data_tags = ['temperature', 'pressure', 'cloud_channel_0', 'cloud_channel_1', 'RCT', 'WT', 'THT'] 
+    # db_data_tags = ['temperature', 'pressure', 'cloud_channel_0', 'cloud_channel_1'] 
 
     # TODO fix this
     def on_exit():
